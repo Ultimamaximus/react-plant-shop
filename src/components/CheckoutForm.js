6ade711ff69b35ac3f6ecf803cd177a63aaa4a2c
Ultimamaximus <82238915+Ultimamaximus@users.noreply.github.com>
2023-07-31 18:49:11 -0700

    I fixed the checkout system bug.

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from 'react';
-import { CardElement, useStripe, useElements, CardNumberElement, CardExpiryElement, CardCvcElement } from '@stripe/react-stripe-js';
+import React, { useState, useContext, useRef } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import { httpsCallable } from 'firebase/functions';
 import { CartContext } from '../context/CartContext';
 import { useNavigate } from 'react-router-dom'; 
@@ -28,10 +28,18 @@ const CARD_ELEMENT_OPTIONS = {
 };
 
 const CheckoutForm = () => {
-  const { cart, clearCart, billingInfo, shippingInfo } = useContext(CartContext);
+  const { cart, clearCart, storeCustomerInfo } = useContext(CartContext);
   const stripe = useStripe();
   const elements = useElements();
   const navigate = useNavigate(); 
+
+  // Declare refs for your input fields
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const addressRef = useRef();
+  const cityRef = useRef();
+  const stateRef = useRef();
+  const zipCodeRef = useRef();
   
   const createStripePaymentIntent = httpsCallable(functionsInstance, 'createStripePaymentIntent');
   
@@ -39,7 +47,7 @@ const CheckoutForm = () => {
   const [processing, setProcessing] = useState(false);
   const [isSameAddress, setIsSameAddress] = useState(true);
 
-  const totalCost = cart.reduce((total, item) => total + (item.price * item.quantity), 0).toFixed(2);
+  const totalCost = cart.reduce((total, item) => total + Math.round(item.price * item.quantity * 100), 0);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -47,10 +55,23 @@ const CheckoutForm = () => {
     if (!stripe || !elements) {
       return;
     }
+
+    // Get billing info from inputs
+    const billingInfo = {
+      firstName: firstNameRef.current.value,
+      lastName: lastNameRef.current.value,
+      address: addressRef.current.value,
+      city: cityRef.current.value,
+      state: stateRef.current.value,
+      zipCode: zipCodeRef.current.value,
+    };
+
+    // Store billing info in context
+    storeCustomerInfo(billingInfo);
     
     setProcessing(true);
     
-    const { data: { clientSecret } } = await createStripePaymentIntent({ amount: parseFloat(totalCost) * 100 });
+    const { data: { clientSecret } } = await createStripePaymentIntent({ amount: totalCost });
     
     const result = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
@@ -63,10 +84,9 @@ const CheckoutForm = () => {
     } else {
       if (result.paymentIntent.status === 'succeeded') {
         const order = {
-          billingInfo,
-          shippingInfo,
+          customerInfo: billingInfo,
           items: cart,
-          total: parseFloat(totalCost),
+          total: totalCost / 100,
         };
         await addDoc(collection(db, "orders"), order);
         clearCart();
@@ -87,21 +107,18 @@ const CheckoutForm = () => {
       <form onSubmit={handleSubmit} className={styles['payment-form']}>
         <h3 className={styles['section-title']}>Payment Details</h3>
         <div className={styles['payment-details']}>
-          <CardNumberElement options={CARD_ELEMENT_OPTIONS} className={styles['card-element']} />
-          <CardExpiryElement options={CARD_ELEMENT_OPTIONS} className={styles['card-element']} />
-          <CardCvcElement options={CARD_ELEMENT_OPTIONS} className={styles['card-element']} />
+          <CardElement options={CARD_ELEMENT_OPTIONS} className={styles['card-element']} />
           <input type="text" placeholder="Name on Card" required className={styles['input']} />
         </div>
 
         <h3 className={styles['section-title']}>Billing Details</h3>
         <div className={styles['billing-details']}>
-          <input type="text" placeholder="First Name" required className={styles['input']} />
-          <input type="text" placeholder="Last Name" required className={styles['input']} />
-          <input type="text" placeholder="Address" required className={styles['input']} />
-          <input type="text" placeholder="City" required className={styles['input']} />
-          <input type="text" placeholder="State" required className={styles['input']} />
-          <input type="text" placeholder="ZIP code" required className={styles['input']} />
-          <input type="text" placeholder="Phone number" required className={styles['input']} />
+        <input ref={firstNameRef} type="text" placeholder="First Name" required className={styles['input']} />
+        <input ref={lastNameRef} type="text" placeholder="Last Name" required className={styles['input']} />
+        <input ref={addressRef} type="text" placeholder="Address" required className={styles['input']} />
+        <input ref={cityRef} type="text" placeholder="City" required className={styles['input']} />
+        <input ref={stateRef} type="text" placeholder="State" required className={styles['input']} />
+        <input ref={zipCodeRef} type="text" placeholder="ZIP code" required className={styles['input']} />
         </div>
 
         <div className={styles['address-check']}>
@@ -125,13 +142,12 @@ const CheckoutForm = () => {
               <input type="text" placeholder="City" required className={styles['input']} />
               <input type="text" placeholder="State" required className={styles['input']} />
               <input type="text" placeholder="ZIP code" required className={styles['input']} />
-              <input type="text" placeholder="Phone number" required className={styles['input']} />
             </div>
           </>
         )}
 
         <div className={styles['confirm-order']}>
-          <p className={styles['total']}>Total: ${totalCost}</p>
+          <p className={styles['total']}>Total: ${(totalCost / 100).toFixed(2)}</p>
           <button type="submit" disabled={!stripe || processing} className={styles['pay-button']}>
             Confirm Order
           </button>
